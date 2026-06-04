88b3e18bc6c575662253d34d0ac59ef5ed0fa7e6
313027-cty <313027@g.clvs.tyc.edu.tw>
2026-06-04 15:07:02 +0800

    Change action name from '側平舉' to '雙手側平舉'

--- 0209v5/app.py
+++ 0209v5/app.py
@@ -3,7 +3,7 @@ from rehab_core import ExerciseConfig, run_app
 
 run_app(
     ExerciseConfig(
-        action_name="側平舉",
+        action_name="雙手側平舉",
         kind="lateral_raise",
         demo_video="media/0514v5.mp4",
         target_reps=5,
